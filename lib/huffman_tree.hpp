f347c96c8d09e39e231ec5ecca4d23f8c1aee010
amr mohamed <amrali.1235@hotmail.com>
2020-06-05 14:31:35 +0200

    creat node function added

--- lib/huffman_tree.hpp
+++ lib/huffman_tree.hpp
@@ -2,19 +2,21 @@
 #define __HuffmanTree__
 
 template<class valueType>
-class huffman_tree
-{
-private:
-    struct node{
+struct node{
         valueType value;
         int frequancy;
         node* left;
         node* right;
-    }; 
-    node* root; 
+    };
+
+template<class valueType>   
+class huffman_tree
+{
+private:
+    node<valueType> * root;
 public:
     huffman_tree(/* args */); 
-    
+    node<valueType> * creatNode(valueType value,int frequancy);
 };
 
 template<class valueType>
@@ -23,7 +25,15 @@ huffman_tree <valueType>::huffman_tree(/* args */)
     
 }
 
-
+template<class valueType>
+node<valueType> * huffman_tree<valueType>::creatNode(valueType value,int frequancy){
+    node<valueType> n;
+    n.value = value;
+    n.frequancy = frequancy;
+    n.left = nullptr;
+    n.right = nullptr;
+    return &n;
+}
 
 
 #endif
