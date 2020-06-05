81920e8448f77924583169054a82f8db8e08c543
amr mohamed <amrali.1235@hotmail.com>
2020-06-05 14:13:04 +0200

    huffman tree class start

--- lib/huffman_tree.hpp
+++ lib/huffman_tree.hpp
@@ -1,4 +1,29 @@
 #ifndef __HuffmanTree__
 #define __HuffmanTree__
 
+template<class valueType>
+class huffman_tree
+{
+private:
+    struct node{
+        valueType value;
+        int frequancy;
+        node* left;
+        node* right;
+    }; 
+    node* root; 
+public:
+    huffman_tree(/* args */); 
+    
+};
+
+template<class valueType>
+huffman_tree <valueType>::huffman_tree(/* args */)
+{
+    
+}
+
+
+
+
 #endif
